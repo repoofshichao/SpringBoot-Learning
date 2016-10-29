e9632bb95ee5d89bd04c4e9d2767f219897e5c4f
repoofshichao <shichaobox@163.com>
2016-10-29 21:37:14 +0800

    init

---
+++ src/main/java/com/cloud/config/ServerApplication.java
@@ -0,0 +1,14 @@
+package com.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
